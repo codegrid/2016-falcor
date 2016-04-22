d6d9edb3f1db9b8518575c06cb986142e24e317a
ykhs <yukihisa.yamada@gmail.com>
2016-04-22 13:33:29 +0900

    client.jsにJSONGraph構造を示すコメントを追加

--- client.js
+++ client.js
@@ -1,5 +1,51 @@
 'use strict';
 
+/*
+Falcor Modelを経由して次のJSONGraph構造を操作できます。
+
+{
+  "entries": {
+    "0": {
+      "_id": <記事ID>,
+      "title": <記事タイトル>,
+      "body": <記事本文>,
+      "authors": [
+        ...
+        {
+          "_id": <著者ID>,
+          "name" <著者名>
+        }
+      ]
+      "tags": [...<タグ>]
+    },
+    ...,
+    "legth": 6
+  },
+  "entriesById": {
+    <記事ID>: {
+      "_id": <記事ID>,
+      "title": <記事タイトル>,
+      "body": <記事本文>,
+      "authors": [
+        ...
+        {
+          "_id": <著者ID>,
+          "name" <著者名>
+        }
+      ]
+      "tags": [...<タグ>]
+    },
+    ...
+  },
+  "authorsById": {
+    <著者ID>: {
+      "_id": <著者ID>,
+      "name": <著者名>
+    }
+  }
+}
+*/
+
 const model = new falcor.Model({
   source: new falcor.HttpDataSource('/model.json')
 });
